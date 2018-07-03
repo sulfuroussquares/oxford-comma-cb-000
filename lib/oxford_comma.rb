6b5dda35ac225d494dfbe5cc79fb3239a71bfe5a
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-03 03:38:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-puts array.join(", ")
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length == 1
+    return array.join
+  else
+    lastWord = array[-1]
+    array.pop
+    newArray = array.join(", ")
+    return newArray + ", and " + lastWord
+  end
 end
