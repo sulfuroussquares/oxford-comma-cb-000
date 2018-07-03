c278f6e3111e6e0677d205874b83ef9700256f49
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-03 03:19:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
-    elsif array.length == 3
-    return array
+  elsif array.length == 1
+    return array.join
   end
 end
